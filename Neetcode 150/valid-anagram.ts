97fd3c4183a6e3d8e16233afb93dc6cdb0987da3
loremus <loremus@proton.me>
2026-09-17 10:36:55 +0530

    better valid anagram solution

--- Neetcode 150/valid-anagram.ts
+++ Neetcode 150/valid-anagram.ts
@@ -5,10 +5,20 @@ class Solution {
    * @return {boolean}
    */
   isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) {
+    const sSize = s.length;
+    if (sSize !== t.length) {
       return false;
     }
 
-    return [...s].sort().join("") == [...t].sort().join("");
+    const map: Map<string, number> = new Map();
+    for (let i = 0; i < sSize; i++) {
+      const sChar = s.charAt(i);
+      const tChar = t.charAt(i);
+
+      map.has(sChar) ? map.set(sChar, map.get(sChar)! + 1) : map.set(sChar, 1);
+      map.has(tChar) ? map.set(tChar, map.get(tChar)! - 1) : map.set(tChar, -1);
+    }
+
+    return Array.from(map.values()).every((item) => item === 0);
   }
 }
